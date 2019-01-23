99d96fee9bc92128767468645cd08863176c1c50
Bartłomiej Wieczorek <b_wieczorek_88@wp.pl>
2019-01-23 21:39:04 +0100

    Initial commit

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int first = int.Parse(tbFrtNumber.Text);
+            int second = int.Parse(tbSecNumber.Text);
+
+            lblCalculateResAdd.Content = first + "+" + second + "=" + (first + second);
+            lblCalculateResSub.Content = first + "-" + second + "=" + (first - second);
+            lblCalculateResMult.Content = first + "x" + second + "=" + (first * second);
+            lblCalculateResDiv.Content = first + "/" + second + "=" + (first / second);
+        }
+    }
+}
